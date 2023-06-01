adb67be0d38b41566adb8ddb4eb326ade38a590f
Jianxuan Cao <jianxuancao@outlook.com>
2023-06-01 15:29:06 +0800

    drink completed

--- entry/src/main/js/default/pages/index/index.js
+++ entry/src/main/js/default/pages/index/index.js
@@ -1,30 +1,42 @@
 export default {
     data: {
         title: "",
+        text: "咋没渴死你",
+        textBank: ["咋没渴死你", "再多喝点吧", "行了，再多就要喝死了"],
         waterCount: 0.0,
         totalWaterCount: 8.0,
         percentage: 0.0
+    },
+    updateText() {
+        this.title = this.waterCount + "/" + this.totalWaterCount;
+        this.percentage = this.waterCount * 100 / this.totalWaterCount;
+        var index = 0;
+        if (this.percentage > 100) {
+            index = 2;
+        } else if (this.percentage > 33) {
+            index = 1;
+        } else {
+            index = 0;
+        }
+
+        this.text = this.textBank[index];
     },
     onInit() {
         this.title = this.waterCount + "/" + this.totalWaterCount;
     },
     plusWater() {
         this.waterCount++;
-
-        this.title = this.waterCount + "/" + this.totalWaterCount;
-        this.percentage = this.waterCount * 100 / this.totalWaterCount;
+        this.updateText();
     },
     minusWater() {
-        this.waterCount--;
-
-        this.title = this.waterCount + "/" + this.totalWaterCount;
-        this.percentage = this.waterCount * 100 / this.totalWaterCount;
+        if (this.waterCount != 0) {
+            this.waterCount--;
+        }
+        this.updateText();
     },
     increaseTotal() {
         this.totalWaterCount++;
-
-        this.title = this.waterCount + "/" + this.totalWaterCount;
-        this.percentage = this.waterCount * 100 / this.totalWaterCount;
+        this.updateText();
     },
     decreaseTotal() {
         this.totalWaterCount--;
@@ -32,13 +44,11 @@ export default {
         if (this.totalWaterCount <= 0) {
             this.totalWaterCount = 1;
         }
-
-        this.title = this.waterCount + "/" + this.totalWaterCount;
-        this.percentage = this.waterCount * 100 / this.totalWaterCount;
+        this.updateText();
     },
     reset() {
         this.waterCount = 0;
-        this.title = this.waterCount + "/" + this.totalWaterCount;
-        this.percentage = this.waterCount * 100 / this.totalWaterCount;
+        this.updateText();
     }
 }
+
